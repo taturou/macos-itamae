c7018fc3bca3a30817c2ba6eccfa3d0eb9903b54
taturou <taturou@gmail.com>
2017-01-04 00:28:57 +0900

    Homebrew-Cask と Homebrew-versions をインストールするシェルスクリプトを作成した。

--- cookbooks/homebrew/recipe.sh
+++ cookbooks/homebrew/recipe.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-# install homebrew
+# install Homebrew
 which brew > /dev/null
 if [ $? != 0 ]; then
   cd ~/.itamae
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+# install Homebrew-Cask
+brew cask --version
+
+# install Homebrew-versions
+brew tap caskroom/versions
