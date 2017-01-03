e693d3bce754fb713356d874627ff9fc4be26af6
taturou <taturou@gmail.com>
2017-01-04 01:16:39 +0900

    itamae の screen レシピを作成した。

---
+++ cookbooks/screen/default.rb
@@ -0,0 +1,16 @@
+require "itamae/plugin/resource/brew"
+
+home="#{node[:home_macos]}/#{node[:user]}"
+itamae_root="#{node[:home_macos]}/#{node[:user]}/#{node[:itamae_root]}"
+
+# install
+brew 'homebrew/dupes/screen' do
+  user "#{node[:user]}"
+end
+
+# screenrc
+link "#{home}/.screenrc" do
+  to "#{itamae_root}/cookbooks/screen/files/home/username/_.screenrc"
+  not_if "test -L #{home}/.screenrc"
+  user "#{node[:user]}"
+end
